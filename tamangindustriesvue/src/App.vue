dfea60d685da494bc3b137213a1b9f7e6b43a898
Sagar Tamang <sagar.bdr0000@gmail.com>
2023-08-20 11:19:29 +0530

    just some stuff...

--- tamangindustriesvue/src/App.vue
+++ tamangindustriesvue/src/App.vue
@@ -1,22 +1,53 @@
 <template>
-  <div>
-    <div class="p-10">
-      <nav class="navbar navbar-light bg-light justify-content-between">
-        <a class="navbar-brand" href="">Navbar</a>
-        <form class="form-inline">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Expand at md</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample04">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown04">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-md-0">
+          <input class="form-control" type="text" placeholder="Search">
         </form>
-      </nav>
-    </div>
+      </div>
+    </nav>
+  
+  <section class="section">
     <router-view/>
+  </section>
+
+    <div class="container">
+     <button type="button" class="btn btn-success">Success</button>
+     <button type="button" class="btn btn-info">Info</button>
+     <button type="button" class="btn btn-warning">Warning</button>
   </div>
+
   <footer>
     This is a footer 
   </footer>
 </template>
 
-<style lang="scss">
+<!-- <style>
 @import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 @import '../node_modules/bootstrap-vue/dist/bootstrap-vue.min.css';
-</style>
+</style> -->
--- tamangindustriesvue/src/main.js
+++ tamangindustriesvue/src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import jquery from 'jquery'
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
+import Bootstrapjs from 'bootstrap/dist/js/bootstrap.js'
+// import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.css'
+// import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.js' 
 
-createApp(App).use(store).use(router).mount('#app')
+
+createApp(App).use(store).use(router).use(jquery).use(Bootstrap).use(Bootstrapjs).mount('#app')
